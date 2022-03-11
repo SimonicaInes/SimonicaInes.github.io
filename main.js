5c965807ee75e3f71dc0b19523fff6846af21c12
Ines <simonica.ines@gmail.com>
2022-03-11 11:38:38 +0200

    C2

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import './style.css'
 
-import * as THREE from '../node_modules/three/build/three.module.js';
+import * as THREE from '../SimonicaInes.github.io/node_modules/three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
