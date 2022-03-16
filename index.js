ef4a73bb78a5362292f224d0ae98bbe072e439bd
Ines <simonica.ines@gmail.com>
2022-03-16 12:41:08 +0200

    doploy 1

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
+// import './index.css'
+
 import * as THREE from 'three'
+import { CubeCamera } from 'three'
+
+
+
+
+const scene = new THREE.Scene()
+const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 )
+
+
+
+const gridHelper = new THREE.GridHelper(100, 10, 0xFFFFFF, 0xFFFFFF)
+// scene.add(gridHelper)
+camera.position.z = 130
+
+camera.position.y = 10
+
+
+//GEOMETRY ADDING
+// const geometry = new THREE.BoxGeometry(5,5,5,100)
+// const material = new THREE.MeshStandardMaterial({color:0x002050})
+// const cube = new THREE.Mesh(geometry,material)
+
+// scene.add(cube);
+
+
+// const geometryF = new THREE.BoxGeometry(5,5,5,100)
+// const materialF = new THREE.MeshStandardMaterial({color:0x002050})
+// const cubeF = new THREE.Mesh(geometryF,materialF)
+// scene.add(cubeF)
+// cubeF.position.setX(20)
+// cubeF.position.setZ(101)
+//END GEOMETRY
+
+const light = new THREE.AmbientLight(0xFFFFFF)
+scene.add(light);
+
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#bg'),
+    alpha:true
+});
+renderer.setClearColor( 0x000000, 0 );
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
+function animate()
+{
+    requestAnimationFrame(animate)
+    renderer.render(scene,camera)
+}
+
+animate()
